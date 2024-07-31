96e5bf0d57885cfca36e08136d7b1445ab51913b
Володимир Слободян <opitral@proton.me>
2024-07-31 19:24:39 +0300

    init project

---
+++ src/test/java/com/opitral/ads/market/api/AdsMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opitral.ads.market.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
