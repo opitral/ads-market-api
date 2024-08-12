4567bea3692b5d8de4388e7db2bda71d250119eb
Володимир Слободян <opitral@proton.me>
2024-08-12 16:56:32 +0300

    make GettableById only integer

--- src/main/java/com/opitral/ads/market/api/common/helpers/GettableById.java
+++ src/main/java/com/opitral/ads/market/api/common/helpers/GettableById.java
@@ -6,13 +6,12 @@ public interface GettableById {
 
     void setId(Integer id);
 
-    default int compareId(Integer i) {
-        if (this.getId() == null && i == null) {
+    default int compareId(int i) {
+        if (this.getId() == null) {
             return 0;
-        } else if (this.getId() == null) {
-            return -1;
         } else {
-            return i == null ? 1 : this.getId().compareTo(i);
+            this.getId();
+            return this.getId() != null ? this.getId().compareTo(i) : -1;
         }
     }
 }
