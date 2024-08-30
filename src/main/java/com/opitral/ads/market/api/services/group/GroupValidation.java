d1d22c48b9b39b6ccc1fe4c01c91059c9cb37282
Volodymyr Slobodian <opitral@proton.me>
2024-08-30 16:15:57 +0300

    fix midnight time

--- src/main/java/com/opitral/ads/market/api/services/group/GroupValidation.java
+++ src/main/java/com/opitral/ads/market/api/services/group/GroupValidation.java
@@ -6,6 +6,8 @@ import com.opitral.ads.market.api.domain.entity.GroupEntity;
 import com.opitral.ads.market.api.exception.ValidationException;
 import com.opitral.ads.market.api.services.BaseValidator;
 
+import java.time.LocalTime;
+
 @Service
 public class GroupValidation extends BaseValidator<GroupEntity> {
 
@@ -17,7 +19,7 @@ public class GroupValidation extends BaseValidator<GroupEntity> {
     public void validForCreate(GroupEntity entity) {
         super.validForCreate(entity);
 
-        if (entity.getWorkingHoursStart().isAfter(entity.getWorkingHoursEnd()))
+        if (entity.getWorkingHoursStart().isAfter(entity.getWorkingHoursEnd())  && !entity.getWorkingHoursEnd().equals(LocalTime.MIDNIGHT))
             throw new ValidationException(GroupEntity.class.getName(), "Время начала роботы не должно быть позже завершения работы");
 
         if (entity.getPostIntervalInMinutes() % 30 != 0) {
@@ -29,7 +31,7 @@ public class GroupValidation extends BaseValidator<GroupEntity> {
     public void validForUpdate(GroupEntity entity) {
         super.validForUpdate(entity);
 
-        if (entity.getWorkingHoursStart().isAfter(entity.getWorkingHoursEnd()))
+        if (entity.getWorkingHoursStart().isAfter(entity.getWorkingHoursEnd()) && !entity.getWorkingHoursEnd().equals(LocalTime.MIDNIGHT))
             throw new ValidationException(GroupEntity.class.getName(), "Время начала роботы не должно быть позже завершения работы");
 
         if (entity.getPostIntervalInMinutes() % 30 != 0) {
